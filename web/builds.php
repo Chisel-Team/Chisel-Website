f461b91b714994dba48fa6d71f74de426ba5c109
tterrag1098 <tterrag1098@gmail.com>
2016-11-26 22:35:20 -0500

    Add navbar and blank page files. Organize site layout

--- web/builds.php
+++ web/builds.php
@@ -2,7 +2,7 @@
 
     include_once 'includes/twig_loader.php';
 
-    $branch = $_GET['branch'] ?: '1.11/dev';
+    $branch = isset($_GET['branch']) ? $_GET['branch'] : '1.11/dev';
     $proj_url = "http://ci.tterrag.com/job/Chisel/branch/" . str_replace('/', '%252F', $branch);
     $proj_api = $proj_url . "/api/json";
 
