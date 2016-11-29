63db5d6fb4c269312ec7d38f5e42b4c5114b251d
tterrag1098 <tterrag1098@gmail.com>
2016-11-29 12:53:26 -0500

    Begin working on home page, only dummy data so far

--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
 <?php
     include_once 'includes/twig_loader.php';
 
-    $loader->show('base.twig');
+    $team = array(
+        array(
+            'name' => 'tterrag',
+            'img' => 'https://avatars2.githubusercontent.com/u/3751664?v=3&s=460'
+        ),
+        array(
+            'name' => 'Drullkus',
+            'img' => 'https://avatars1.githubusercontent.com/u/5010174?v=3&s=460'
+        ),
+        array(
+            'name' => 'minecreatr',
+            'img' => 'https://avatars3.githubusercontent.com/u/6035929?v=3&s=460'
+        )
+    );
+    $loader->show('home.twig', array('team' => $team));
 ?>
